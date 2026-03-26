a50bfb9f87c49a8117b26a1d51ea77fed8140012
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2026-03-26 16:22:05 +0800

    feat: add Harness Engine Environment

---
+++ harness_engine/environment/__init__.py
@@ -0,0 +1,9 @@
+from .base_environment import BaseEnvironment
+from .feedback import FeedbackCollector
+from .rollout import RolloutExecutor
+
+__all__ = [
+    "BaseEnvironment",
+    "FeedbackCollector",
+    "RolloutExecutor"
+]
